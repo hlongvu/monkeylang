3a7644db1e7dfaed0ef23e5dea030121257181d3
Long Vu <hlongvu0201@gmail.com>
2018-10-27 15:09:54 +0700

    return statement

--- monkey/ast/return_statement.go
+++ monkey/ast/return_statement.go
@@ -6,3 +6,9 @@ type ReturnStatement struct {
 	Token token.Token
 	ReturnValue Expression
 }
+
+func (rs *ReturnStatement) statementNode(){}
+
+func (rs *ReturnStatement) TokenLiteral() string{
+	return rs.Token.Literal
+}

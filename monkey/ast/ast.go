c70d1f4ea1de479041acbc7b1b71f12d675fce4c
KLGT <hlongvu0201@gmail.com>
2018-10-29 14:52:26 +0700

    ast CallExpression

--- monkey/ast/ast.go
+++ monkey/ast/ast.go
@@ -204,6 +204,35 @@ func (f *FunctionLiteral) expressionNode() {}
 
 
 
+//CallExpression
+
+type CallExpression struct {
+	Token token.Token
+	Function Expression // identifier or function literal
+	Arguments []Expression
+}
+
+func (c *CallExpression) TokenLiteral() string {
+	return c.Token.Literal
+}
+
+func (c *CallExpression) String() string {
+	var out bytes.Buffer
+	arguments := []string{}
+
+	for _, a := range c.Arguments{
+		arguments = append(arguments, a.String())
+	}
+
+	out.WriteString(c.Function.String())
+	out.WriteString("(")
+	out.WriteString(strings.Join(arguments, ","))
+	out.WriteString(")")
+	return out.String()
+}
+
+func (c *CallExpression) expressionNode() {}
+
 
 
 
